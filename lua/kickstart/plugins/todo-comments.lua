d97a91b41a2858cc37743588c2152cecb4edee37
VimUser-sharp <fronterlinemail@tutamail.com>
2025-07-25 16:41:05 -0300

    Delete lua/kickstart directory

--- lua/kickstart/plugins/todo-comments.lua
+++
@@ -1,4 +0,0 @@
--- Highlight todo, notes, etc in comments/ coloca cor de destaque em notas,todo e outros special comments
-return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-}
